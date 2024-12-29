24597b47af1672a6025ab29febd4f24ef1f70a42
Mohamed Akeel <akeelaslamcse@gmail.com>
2024-12-29 20:30:01 +0530

    added the offer component and New collections to the shop page

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,4 +1,6 @@
 import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import Offers from "../Components/Offers/Offers";
 import Popular from "../Components/Popular/Popular";
 
 const Shop = () => {
@@ -6,6 +8,8 @@ const Shop = () => {
     <div>
       <Hero />
       <Popular />
+      <Offers />
+      <NewCollections />
     </div>
   );
 };

9dafcdca2a6955b3d8764aa2b3839602037e68f5
Mohamed Akeel <akeelaslamcse@gmail.com>
2024-12-31 00:48:42 +0530

    CartItems+Nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,39 +1,76 @@
-import { useState } from "react";
+import { useContext, useRef, useState } from "react";
 import "./Navbar.css";
-import logo from "../Assets/Frontend_Assets/Logo.png";
+import logo from "../Assets/Frontend_Assets/logo.png";
 import cart_icon from "../Assets/Frontend_Assets/cart_icon.png";
+import nav_dropdown from "../Assets/Frontend_Assets/nav_dropdown.png";
 import { Link } from "react-router-dom";
+import { CartContext } from "../../Context/CartContext";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
+  const { getTotalCartItems } = useContext(CartContext);
+  const menuRef = useRef();
+
+  const dropdown_toggle = (e) => {
+    menuRef.current.classList.toggle("nav-menu-visible");
+    e.target.classList.toggle("open");
+  };
+
   return (
     <div className="navbar">
-      <div className="nav-logo">
+      <Link
+        style={{ textDecoration: "none" }}
+        to="/"
+        onClick={() => {
+          setMenu("shop");
+        }}
+        className="nav-logo"
+      >
         <img src={logo} alt="" />
         <p>SHOPPER</p>
-      </div>
-      <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>
+      </Link>
+      <img
+        onClick={dropdown_toggle}
+        className="nav-dropdown"
+        src={nav_dropdown}
+        alt=""
+      />
+      <ul ref={menuRef} className="nav-menu">
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
           <Link style={{ textDecoration: "none" }} to="/">
             Shop
           </Link>
           {menu === "shop" ? <hr /> : <></>}
         </li>
-
-        <li onClick={() => setMenu("men")}>
+        <li
+          onClick={() => {
+            setMenu("mens");
+          }}
+        >
           <Link style={{ textDecoration: "none" }} to="/mens">
             Men
-          </Link>{" "}
-          {menu === "men" ? <hr /> : <></>}
+          </Link>
+          {menu === "mens" ? <hr /> : <></>}
         </li>
-
-        <li onClick={() => setMenu("women")}>
-          <Link style={{ textDecoration: "none" }} to="/womens">
+        <li
+          onClick={() => {
+            setMenu("womens");
+          }}
+        >
+          <Link style={{ textDecoration: "none" }} to="womens">
             Women
-          </Link>{" "}
-          {menu === "women" ? <hr /> : <></>}
+          </Link>
+          {menu === "womens" ? <hr /> : <></>}
         </li>
-        <li onClick={() => setMenu("kids")}>
+        <li
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
           <Link style={{ textDecoration: "none" }} to="/kids">
             Kids
           </Link>
@@ -44,11 +81,10 @@ const Navbar = () => {
         <Link style={{ textDecoration: "none" }} to="/login">
           <button>Login</button>
         </Link>
-
         <Link style={{ textDecoration: "none" }} to="/cart">
           <img src={cart_icon} alt="" />
         </Link>
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count">{getTotalCartItems()}</div>
       </div>
     </div>
   );
